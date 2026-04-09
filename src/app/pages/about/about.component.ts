65a274ae134438dfe259409c28ecc53f2dab3f0c
Ye Min <ye-min@outlook.com>
2026-04-09 17:22:18 +0800

    feat: update build info script and integrate versioning in about component

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import packageInfo from '../../../../package.json';
+import { buildTimestamp } from '../../build-info';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
-export class AboutComponent { }
+export class AboutComponent {
+  version = packageInfo.version;
+  buildTime = buildTimestamp;
+}
